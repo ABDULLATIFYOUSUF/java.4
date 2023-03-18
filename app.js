ac796450951130efe2f96c21f8cf9f25c1ca1186
latif <latifyousuf513@gmail.com>
2023-03-18 15:00:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// CHP # 21-25
+// Q # 1
+// var firstName = prompt("enter your first name");
+// var lastName = prompt("enter your last name");
+// var fullName = firstName + " " + lastName;
+// alert("welcome" + " " + fullName);
+
+// Q # 2
+// var phoneModel = prompt("your favourite mobile phone");
+// var length = phoneModel.length;
+// document.write("My favorite phone is:" + " " + phoneModel);
+// document.write("</br>")
+// document.write("Length of string:" + " " + length)
