f8c179b4dfede44a4d6153ea5788262a215d78d5
latif <latifyousuf513@gmail.com>
2023-03-18 17:10:44 +0500

    first commit

--- app.js
+++ app.js
@@ -11,3 +11,71 @@
 // document.write("My favorite phone is:" + " " + phoneModel);
 // document.write("</br>")
 // document.write("Length of string:" + " " + length)
+
+// Q # 3
+// var string = "Pakistani"
+// document.write("Index of 'n':" + " " + string.indexOf("n"))
+
+// Q # 4
+// var string = "Hello World"
+// document.write("Last index of 'l':" + " " + string.lastIndexOf("l"))
+
+// Q # 5
+// var string = "Pakistani"
+// document.write("Character at Index 3:" + " " + string[3])
+
+// Q # 6
+// var firstName = prompt("enter your first name");
+// var lastName = prompt("enter your last name");
+// var fullName = firstName + " " + lastName;
+// alert("welcome" + " " + fullName);
+
+// Q # 7
+// var city = "Hyderabad";
+// document.write("City: " + city);
+// city = city.replace("Hyder", "Islam");
+// document.write("<br>")
+// document.write("After replacement: " + city);
+
+// Q # 8
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// document.write(message)
+// message = message.replace(/and/g, "&")
+// document.write("<br>")
+// document.write(message)
+
+// Q # 9
+// var string = "472"
+// document.write("string: " + string)
+// document.write("<br>")
+// var number = parseInt(string)
+// document.write("number: " + number)
+
+// Q # 10
+// var userInput = prompt("write something")
+// document.write("User Input: " + userInput)
+// document.write("<br>")
+// document.write("Upper Case: " + userInput.toUpperCase())
+
+// Q # 11
+// var userInput = prompt("write something")
+// document.write("User Input: " + userInput)
+// document.write("<br>")
+// var firstLetter = userInput[0].toUpperCase()
+// var otherLetters = userInput.slice(1).toLowerCase()
+// var titleCase = firstLetter + otherLetters
+// document.write("Title Case: " + titleCase)
+
+// Q # 12
+// var num = 35.36;
+// document.write(num)
+// document.write("<br>")
+// var string = num.toString()
+// document.write(string)
+
+// Q # 13
+
+
+
+
+
